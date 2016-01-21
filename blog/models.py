038a3c8d529cb4f7feee23fec1c22210e1ca4e79
Ben Kirkup <Ben Kirkup>
2016-01-21 14:36:22 +0000

    Modifies the post_list template to diplay post model, adds abstract field to post model

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=255, blank=False)
+    abstract = models.CharField(max_length=400, blank=True, null=True)
     text = models.TextField(blank=False)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=False, null=True)
